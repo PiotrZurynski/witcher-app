d624ea3eb19b91ba0aeccbf78f8b4c79aaa61d23
PiotrZurynski <pewdiejanusz@gmail.com>
2025-10-15 19:08:51 +0200

    Created model about Monsters and makemigration

--- witcher/contracts/models.py
+++ witcher/contracts/models.py
@@ -15,4 +15,31 @@ class Town(models.Model):
     realm=models.ForeignKey(Realm,on_delete=models.CASCADE)
 
     class Meta:
-        ordering=["realm", "name"]    
+        ordering=["realm", "name"]
+    def __str__(self):
+        return self.name + " " + self.realm
+
+class Monster(models.Model):
+    CATEGORY_CHOICES=[
+        ("BES","Bestie"),
+        ("DRA","Drakonidy"),
+        ("HYB","Hybrydy"),
+        ("INS","Insektoidy"),
+        ("ISTM","Istoty Magiczne"),
+        ("ISTP","Istoty przeklęte"),
+        ("OGR","Ogrowate"),
+        ("REL","Relikty"),
+        ("TRU","Trupojady"),
+        ("UPI","Upiory"),
+        ("WAM","Wampiry"),
+        ("UNK","Nieznane")
+    ]
+    name=models.CharField(max_length=70)
+    category=models.CharField(max_length=5,choices=CATEGORY_CHOICES,default="TRU")
+    description=models.TextField(blank=True)
+
+    class Meta:
+        ordering=["category","name"]
+
+    def __str__(self):
+        return self.category + " - " + self.name
