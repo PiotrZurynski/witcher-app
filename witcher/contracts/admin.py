f0b1b7714c689e43e1d5cc6f7af69382f4e8a3a3
PiotrZurynski <pewdiejanusz@gmail.com>
2025-10-15 18:42:13 +0200

    Created two models with fields and makemigration

--- witcher/contracts/admin.py
+++ witcher/contracts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Realm, Town
+
+admin.site.register(Realm)
+admin.site.register(Town)
--- witcher/contracts/models.py
+++ witcher/contracts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Realm(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+    description=models.TextField(blank=True)
+
+    class Meta:
+        ordering=["name"]
+    def __str__(self):
+        return self.name
+
+class Town(models.Model):
+    name=models.CharField(max_length=100)
+    realm=models.ForeignKey(Realm,on_delete=models.CASCADE)
+
+    class Meta:
+        ordering=["realm", "name"]    
