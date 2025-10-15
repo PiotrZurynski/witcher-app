351b78a28ce2533cd3eed02ee5bb019803526378
PiotrZurynski <pewdiejanusz@gmail.com>
2025-10-15 22:36:38 +0200

    Modificated navbar in base.html and created login.html to login

--- witcher/contracts/views.py
+++ witcher/contracts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Contract
 from django.views.generic.edit import CreateView
 from .forms import ContractCreateForm
+from django.urls import reverse_lazy
 # Create your views here.
 def home(request):
     return render(request,"contracts/home.html")
@@ -12,3 +13,4 @@ class ContractCreate(CreateView):
     model=Contract
     template_name='contracts/contract_create_form.html'
     form_class=ContractCreateForm
+    success_url=reverse_lazy('home')
